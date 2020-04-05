fb6da69fa65e5201857baf70c74ad7ac8d6c8382
liliu <liliu@signcc.com>
2020-04-05 18:41:41 +0800

    秒杀系统设计

---
+++ src/test/java/com/ztk/miaosha/code/CodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ztk.miaosha.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
